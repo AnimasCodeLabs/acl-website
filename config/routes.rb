a06814f77f9ecff91ef3ca4b9f6f4a9e563baea1
Aaron Renner <aaron@animascodelabs.com>
2013-10-03 08:16:54 -0600

    Fixed merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 AclWebsite::Application.routes.draw do
-  get '/contact' => 'high_voltage/pages#show', id: 'contact', as: :contact
-<<<<<<< HEAD
 
   # Although show isn't implemented, it was needed to prevent
   # undefined method error messages_url
   resources :messages, only: [:create, :show], format: :json
-=======
->>>>>>> 14a812f222fb2e1def69dad6bc919abe330b969e
 
+  get '/contact' => 'high_voltage/pages#show', id: 'contact', as: :contact
   root to: 'high_voltage/pages#show', id: 'home'
 end
