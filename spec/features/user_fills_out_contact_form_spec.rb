3cbe4cbd42e3f4dae5ac6b156ba1a2434e0bf4fb
Aaron Renner <aaron@animascodelabs.com>
2013-10-03 09:06:48 -0600

    Refactored contact form feature spec

--- spec/features/user_fills_out_contact_form_spec.rb
+++ spec/features/user_fills_out_contact_form_spec.rb
@@ -4,18 +4,26 @@ feature 'contact form' do
   scenario 'user fills out contact form', js: true do
     visit contact_path
 
+    fill_out_contact_form
+
+    user_sees_thank_you_message
+
+    sales_gets_email
+  end
+
+  def fill_out_contact_form
     fill_in 'message_name', with:  'John Doe'
     fill_in 'message_email', with: 'jdoe@example.com'
     fill_in 'message_body', with: 'Hello World'
 
     click_button 'GET STARTED'
-
-    user_sees_thank_you_message
-
-    expect(last_email).to deliver_to("sales@animascodelabs.com")
   end
 
   def user_sees_thank_you_message
     expect(page).to have_content("Thank you")
+  end
+
+  def sales_gets_email
+    expect(last_email).to deliver_to("sales@animascodelabs.com")
   end
 end
