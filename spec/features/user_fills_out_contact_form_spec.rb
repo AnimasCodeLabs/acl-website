e8f6a8e6ebed1dd74bec5a871f137baa68a57db9
Jason Sutter <jason@animascodelabs.com>
2014-12-23 13:13:36 -0700

    Responsive work

--- spec/features/user_fills_out_contact_form_spec.rb
+++ spec/features/user_fills_out_contact_form_spec.rb
@@ -11,19 +11,19 @@
 #     #sales_gets_email
 #   end
 #
-#   # def fill_out_contact_form
-#   #   fill_in 'first-name', with: 'John'
-#   #   fill_in 'email', with: 'jdoe@example.com'
-#   #   fill_in 'phone', with: '970-403-5085'
-#   #
-#   #   click_button 'GET STARTED'
-#   # end
+#    def fill_out_contact_form
+#      fill_in 'first-name', with: 'John'
+#      fill_in 'email', with: 'jdoe@example.com'
+#      fill_in 'phone', with: '970-403-5085'
 #
-#   # def user_sees_thank_you_message
-#   #   expect(page).to have_content("Thank you")
-#   # end
-#   #
-#   # def sales_gets_email
-#   #   expect(last_email).to deliver_to("sales@animascodelabs.com")
-#   # end
+#      click_button 'GET STARTED'
+#    end
+#
+#    def user_sees_thank_you_message
+#      expect(page).to have_content("Thank you")
+#    end
+#
+#    def sales_gets_email
+#      expect(last_email).to deliver_to("sales@animascodelabs.com")
+#    end
 # end
